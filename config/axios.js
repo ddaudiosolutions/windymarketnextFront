bb14b38fc4bc189487c5b3369c61c18ff7c5a967
David_cladera <dcladera@externos.itnow.es>
2024-07-23 17:39:21 +0200

    fix cliente axios localhost

--- config/axios.js
+++ config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const clienteAxios = axios.create({
   headers: {},
-  baseURL: 'http://localhost:4000/api/',
+  baseURL: process.env.NEXT_BACKEND_URL,
 });
 
 export default clienteAxios;
